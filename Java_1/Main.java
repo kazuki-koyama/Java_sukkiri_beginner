128eec597bee6069bdf91976546f185c0c6ab3ee
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-11 10:37:19 +0900

    [add] exercise 1

---
+++ Java_1/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// 練習問題1-2
+		int a = 3; int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3 横幅5 の長方形の面積は、" + c);
+		
+		// 練習問題1-3
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		final double PI = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+}
