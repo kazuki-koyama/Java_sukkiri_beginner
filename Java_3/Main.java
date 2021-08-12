07fc3e30b9c11fa72a39c630da123e65d3cb7337
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-12 21:50:28 +0900

    [add] exercise 3-3

---
+++ Java_3/Main.java
@@ -0,0 +1,18 @@
+public class Main { 
+  public static void main(String[] args) {
+    // 練習問題3-3
+    int isHungry = 1;
+    String food = "おにぎり";
+    System.out.println("こんにちは");
+      if (isHungry == 0) {
+        System.out.println("お腹がいっぱいです");
+      } else {
+        System.out.println("はらぺこです");
+      }
+      if (isHungry == 1) {
+        System.out.println(food + "をいただきます");
+      }
+      System.out.println("ごちそうさまでした");
+    }
+  }
+}
