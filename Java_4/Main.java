05bacea0bbdc3088e46482e4dc61561f10e38dda
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-13 00:00:00 +0900

    [add] Java_4 - for statement of array

---
+++ Java_4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] seq = new int [10];
+    char[] base = {'A', 'T', 'G', 'C'};
+
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+      System.out.println(base[seq[i]] + " ");
+    }
+  }
+}
