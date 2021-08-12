2a67d7692553a56a92c01721711b435ec1a39369
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-12 22:11:23 +0900

    [add] exercise 3-6

--- Java_3/Main.java
+++ Java_3/Main.java
@@ -15,22 +15,37 @@ public class Main {
     //   System.out.println("ごちそうさまでした");
     // }
 
-    // 練習問題3-4
-    System.out.print("[メニュー]1:検索 2:登録 3:削除 4:変更>");
-    int selected = new java.util.Scanner(System.in).nextInt();
-    switch(selected) {
-      case 1:
-        System.out.println("検索");
+    // 練習問題3-5
+    // System.out.print("[メニュー]1:検索 2:登録 3:削除 4:変更>");
+    // int selected = new java.util.Scanner(System.in).nextInt();
+    // switch(selected) {
+    //   case 1:
+    //     System.out.println("検索");
+    //     break;
+    //   case 2:
+    //     System.out.println("登録");
+    //     break;
+    //   case 3:
+    //     System.out.println("削除");
+    //     break;
+    //   case 4:
+    //     System.out.println("変更");
+    //     break;
+    // }
+
+    // 練習問題3-6
+    System.out.print("数当てゲーム");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0〜9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (num == ans) {
+        System.out.println("アタリ！");
         break;
-      case 2:
-        System.out.println("登録");
-        break;
-      case 3:
-        System.out.println("削除");
-        break;
-      case 4:
-        System.out.println("変更");
-        break;
+      } else {
+        System.out.println("違います");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
 }
