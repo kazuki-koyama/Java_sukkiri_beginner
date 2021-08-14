61b121e83d6819c7b454a4661f2ac493dd7279dc
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-14 09:23:13 +0900

    [add] exercise 4-4

--- Java_4/Main.java
+++ Java_4/Main.java
@@ -1,11 +1,21 @@
 public class Main {
   public static void main(String[] args) {
-    int[] seq = new int [10];
-    char[] base = {'A', 'T', 'G', 'C'};
+    // int[] seq = new int [10];
+    // char[] base = {'A', 'T', 'G', 'C'};
 
-    for (int i = 0; i < seq.length; i++) {
-      seq[i] = new java.util.Random().nextInt(4);
-      System.out.println(base[seq[i]] + " ");
+    // for (int i = 0; i < seq.length; i++) {
+    //   seq[i] = new java.util.Random().nextInt(4);
+    //   System.out.println(base[seq[i]] + " ");
+    // }
+
+    // 練習問題4-4
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int n : numbers) {
+      if(n == input) {
+        System.out.println("アタリ!");
+      }
     }
   }
 }
