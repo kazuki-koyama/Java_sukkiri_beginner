d7ac9f433fe6d6a277716190ab27df85561c4394
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-15 01:00:00 +0900

    [add] exercise 5-4

--- Java_5/Main.java
+++ Java_5/Main.java
@@ -3,10 +3,16 @@ import org.w3c.dom.Text;
 public class Main {
   public static void main(String[] args) {
     // introduceOneself();
+
     // String title = "お誘い";
-    String address = "user@example.com";
-    String text = "今度、飲みに行きませんか？";
-    email(address, text);
+    // String address = "user@example.com";
+    // String text = "今度、飲みに行きませんか？";
+    // email(address, text);
+
+    double triangleArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積：" + triangleArea + "平方cm");
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("円の面積：" + circleArea + "平方cm");
   }
 
   // 練習問題5-1
@@ -21,15 +27,27 @@ public class Main {
   // }
 
   // 練習問題5-2
-  public static void email(String title, String address, String text) {
-    System.out.println(address + "に、以下のメールを送信しました");
-    System.out.println("件名：" + title);
-    System.out.println("本文：" + text);
+  // public static void email(String title, String address, String text) {
+  // System.out.println(address + "に、以下のメールを送信しました");
+  // System.out.println("件名：" + title);
+  // System.out.println("本文：" + text);
+  // }
+
+  // 練習問題5-3
+  // public static void email(String address, String text) {
+  // System.out.println(address + "に、以下のメールを送信しました");
+  // System.out.println("件名：無題");
+  // System.out.println("本文：" + text);
+  // }
+
+  // 練習問題5-4
+  public static double calcTriangleArea(double bottom, double height) {
+    double area = (bottom * height) / 2;
+    return area;
   }
 
-  public static void email(String address, String text) {
-    System.out.println(address + "に、以下のメールを送信しました");
-    System.out.println("件名：無題");
-    System.out.println("本文：" + text);
+  public static double calcCircleArea(double radius) {
+    double area = radius * radius * 3.14;
+    return area;
   }
 }
