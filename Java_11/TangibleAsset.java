144f0579d46a9be7141238f7269468dc09d34041
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-26 10:16:58 +0900

    [add] exercise 11-4

--- Java_11/TangibleAsset.java
+++ Java_11/TangibleAsset.java
@@ -1,5 +1,6 @@
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
   String color;
+  double weight;
 
   public TangibleAsset(String name, int price, String color) {
     super(name, price);
@@ -8,6 +9,14 @@ public abstract class TangibleAsset extends Asset {
 
   public String getColor() {
     return this.color;
+  }
+
+  public double getWeight() {
+    return this.weight;
+  }
+
+  public void setWeight(double weight) {
+    this.weight = weight;
   }
 
 }
