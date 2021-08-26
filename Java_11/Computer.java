7080cc73631748672b336b196648c1427274bab6
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-26 10:02:52 +0900

    [add] abstract class for exercise 11-1

--- Java_11/Computer.java
+++ Java_11/Computer.java
@@ -1,30 +1,13 @@
-public class Computer {
-  String name;
-  int price;
-  String color;
+public class Computer extends TangibleAsset {
   String makerName;
 
   // コンストラクタ
   public Computer(String name, int price, String color, String makerName) {
-    this.name = name;
-    this.price = price;
-    this.color = color;
+    super(name, price, color);
     this.makerName = makerName;
   }
 
   // メソッド
-  public String getName() {
-    return this.name;
-  }
-
-  public int getPrice() {
-    return this.price;
-  }
-
-  public String getColor() {
-    return this.color;
-  }
-
   public String getMakerName() {
     return this.makerName;
   }
---
+++ Java_11/TangibleAsset.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.NameList;
+
+public abstract class TangibleAsset {
+  String name;
+  int price;
+  String color;
+
+  public TangibleAsset(String name, int price, String color) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public String getColor() {
+    return this.color;
+  }
+
+}
