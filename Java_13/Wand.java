e1313b43da20294ee42872a029d515f841475fbb
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-08-27 10:05:12 +0900

    [add] exercise 13-3

--- Java_13/Wand.java
+++ Java_13/Wand.java
@@ -1,4 +1,20 @@
 public class Wand {
-  private String name; // 杖の名前
-  private double power; // 杖の魔力
+  private String name;
+  private double power;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPower() {
+    return this.power;
+  }
+
+  public void setPower(double power) {
+    this.power = power;
+  }
 }
