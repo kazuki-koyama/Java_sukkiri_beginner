7cc9b8fea7ef459d69182641ee03c6e95f88359e
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-01 09:34:36 +0900

    [add] exercise 16-3

--- Java_16/Main.java
+++ Java_16/Main.java
@@ -12,13 +12,24 @@ public class Main {
     // }
 
     // 練習問題16-2
+    // Hero h1 = new Hero("斎藤");
+    // Hero h2 = new Hero("鈴木");
+    // List<Hero> heroes = new ArrayList<Hero>();
+    // heroes.add(h1);
+    // heroes.add(h2);
+    // for (Hero h : heroes) {
+    // System.out.println(h.getName());
+    // }
+
+    // 練習問題16-3
     Hero h1 = new Hero("斎藤");
     Hero h2 = new Hero("鈴木");
-    List<Hero> heroes = new ArrayList<Hero>();
-    heroes.add(h1);
-    heroes.add(h2);
-    for (Hero h : heroes) {
-      System.out.println(h.getName());
+    Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+    heroes.put(h1, 3);
+    heroes.put(h2, 7);
+    for (Hero key : heroes.keySet()) {
+      int value = heroes.get(key);
+      System.out.println(key.getName() + "が倒した敵=" + value);
     }
   }
 }
