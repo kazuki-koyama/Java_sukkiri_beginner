aaf0a59574e785427954e8e50ee0429725795d6b
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-01 09:27:39 +0900

    [add] exercise 16-2

--- Java_16/Main.java
+++ Java_16/Main.java
@@ -2,13 +2,23 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Map<String, Integer> prefs = new HashMap<String, Integer>();
-    prefs.put("京都府", 255);
-    prefs.put("東京都", 1261);
-    prefs.put("熊本県", 182);
-    for (String key : prefs.keySet()) {
-      int value = prefs.get(key);
-      System.out.println(key + "の人口は、" + value);
+    // Map<String, Integer> prefs = new HashMap<String, Integer>();
+    // prefs.put("京都府", 255);
+    // prefs.put("東京都", 1261);
+    // prefs.put("熊本県", 182);
+    // for (String key : prefs.keySet()) {
+    // int value = prefs.get(key);
+    // System.out.println(key + "の人口は、" + value);
+    // }
+
+    // 練習問題16-2
+    Hero h1 = new Hero("斎藤");
+    Hero h2 = new Hero("鈴木");
+    List<Hero> heroes = new ArrayList<Hero>();
+    heroes.add(h1);
+    heroes.add(h2);
+    for (Hero h : heroes) {
+      System.out.println(h.getName());
     }
   }
 }
