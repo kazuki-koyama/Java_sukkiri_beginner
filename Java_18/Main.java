31d288527de1c5307f2e2bb814d73491b048117b
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-03 08:03:48 +0900

    [add] 18-3

--- Java_18/Main.java
+++ Java_18/Main.java
@@ -1,20 +1,31 @@
-import java.io.*;
+import java.io.InputStream;
+import java.net.URL;
+// import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    FileWriter fw = new FileWriter("data.txt");
-    fw.write('そ');
-    fw.write('れ');
-    fw.write('で');
-    fw.write('は');
-    fw.close();
+    // FileWriter fw = new FileWriter("data.txt");
+    // fw.write('そ');
+    // fw.write('れ');
+    // fw.write('で');
+    // fw.write('は');
+    // fw.close();
 
-    FileReader fr = new FileReader("data.txt");
-    int input = fr.read();
-    while (input != -1) {
-      System.out.print((char) input);
-      input = fr.read();
+    // FileReader fr = new FileReader("data.txt");
+    // int input = fr.read();
+    // while (input != -1) {
+    // System.out.print((char) input);
+    // input = fr.read();
+    // }
+    // fr.close();
+
+    URL u = new URL("https://book.impress.co.jp/");
+    InputStream is = u.openStream();
+    int i = is.read();
+    while (i != -1) {
+      char c = (char) i;
+      System.out.print(c);
+      i = is.read();
     }
-    fr.close();
   }
 }
