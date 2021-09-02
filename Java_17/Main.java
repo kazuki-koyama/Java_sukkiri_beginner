272b2fdfe63843995e15a1da5d0d82f7d6e3e52f
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-02 09:46:42 +0900

    [add] exercise 17-3

--- Java_17/Main.java
+++ Java_17/Main.java
@@ -1,14 +1,22 @@
 public class Main {
 
   public static void main(String[] args) {
+    // 練習問題17-2
+    // try {
+    // String s = null;
+    // System.out.println(s.length());
+    // } catch (NullPointerException e) {
+    // System.out.println("NullPointerException例外をcatchしました");
+    // System.out.println("--スタックトレース（ここから）--");
+    // e.printStackTrace();
+    // System.out.println("--スタックトレース（ここまで）--");
+    // }
+
+    // 練習問題17-3
     try {
-      String s = null;
-      System.out.println(s.length());
-    } catch (NullPointerException e) {
-      System.out.println("NullPointerException例外をcatchしました");
-      System.out.println("--スタックトレース（ここから）--");
-      e.printStackTrace();
-      System.out.println("--スタックトレース（ここまで）--");
+      int i = Integer.parseInt("三");
+    } catch (NumberFormatException e) {
+      System.out.println("NumberFormatException例外をcatchしました");
     }
   }
 }
