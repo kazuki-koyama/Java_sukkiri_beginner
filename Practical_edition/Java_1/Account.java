705ad2a260b9e9817e41c65c9b01ea56f7796da1
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-04 08:34:01 +0900

    [add] implements Comparable interface for sort

--- Practical_edition/Java_1/Account.java
+++ Practical_edition/Java_1/Account.java
@@ -1,14 +1,29 @@
-public class Account {
-  String accountNo;  // 口座番号（先頭に空白が入ることもある）
+public class Account implements Comparable<Account> {
+  // String accountNo; // 口座番号（先頭に空白が入ることもある）
+  int number; // 口座番号
+  int zandaka; // 残高
 
-  public boolean equals(Object o) {
-    if (o == this) return true;
-    if (o == null) return false;
-    if(!(o instanceof Account)) return false;
-    Account r = (Account)o;
-    if (!this.accountNo.trim().equals(r.accountNo.trim())) {
-      return false;
+  public int compareTo(Account obj) {
+    if (this.number < obj.number) {
+      return -1;
     }
-    return true;
+    if (this.number > obj.number) {
+      return 1;
+    }
+    return 0;
   }
+
+  // public boolean equals(Object o) {
+  // if (o == this)
+  // return true;
+  // if (o == null)
+  // return false;
+  // if (!(o instanceof Account))
+  // return false;
+  // Account r = (Account) o;
+  // if (!this.accountNo.trim().equals(r.accountNo.trim())) {
+  // return false;
+  // }
+  // return true;
+  // }
 }
--- Practical_edition/Java_1/Main.java
+++ Practical_edition/Java_1/Main.java
@@ -1,20 +1,46 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
-    Account a1 = new Account();
-    a1.accountNo = "001";
+    // equalsメソッドのオーバーライド
+    // Account a1 = new Account();
+    // a1.accountNo = "001";
 
-    // (1)自分自身が引数として渡された場合、無条件でtrueを返す。
-    System.out.println(a1.equals(a1));
-    // (2)nullが引数として渡された場合、無条件でfalseを返す。
-    System.out.println(a1.equals(null));
-    // (3)型が異なるならば、falseを返す。
-    String s1 = "";
-    System.out.println(a1.equals(s1));
-    // (4)型が同じなら、2つのインスタンスが持つフィールド同士を比較して等価か判定する
+    // System.out.println(a1.equals(a1));
+    // System.out.println(a1.equals(null));
+    // String s1 = "";
+    // System.out.println(a1.equals(s1));
+    // Account a2 = new Account();
+    // a2.accountNo = "002";
+    // System.out.println(a1.equals(a2));
+    // a2.accountNo = " 001";
+    // System.out.println(a1.equals(a2));
+
+    // 配列の等価判定
+    // int[] a = { 1, 2, 3, 4, 5 };
+    // int[] b = { 1, 2, 3, 4, 5 };
+    // System.out.println("誤った判定：" + a.equals(b));
+    // System.out.println("正しい判定：" + Arrays.equals(a, b));
+
+    // 並び替え
+    List<Account> list = new ArrayList<>();
+    Account a1 = new Account();
     Account a2 = new Account();
-    a2.accountNo = "002";
-    System.out.println(a1.equals(a2));
-    a2.accountNo = "  001";
-    System.out.println(a1.equals(a2));
+    a1.number = 200;
+    a2.number = 100;
+    list.add(a1);
+    list.add(a2);
+
+    System.out.println("ソート前：");
+    for (Account a : list) {
+      System.out.println(a.number);
+    }
+
+    Collections.sort(list);
+
+    System.out.println("ソート後：");
+    for (Account a : list) {
+      System.out.println(a.number);
+    }
   }
 }
