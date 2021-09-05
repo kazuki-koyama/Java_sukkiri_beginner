d78965124f8bbd6dbee4405a8017285d5a1ee17c
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-06 00:00:00 +0900

    [add] AccountType.java

--- Practical_edition/Java_2/AccountType.java
+++ Practical_edition/Java_2/AccountType.java
@@ -3,3 +3,9 @@
 public enum AccountType {
   FUTSU, TOUZA, TEIKI;
 }
+
+Collections.sort(list, new Comparator<Account>() {
+  public int compare(Account x, Account y) {
+    return (x.zandaka - y.zandaka);
+  }
+}
