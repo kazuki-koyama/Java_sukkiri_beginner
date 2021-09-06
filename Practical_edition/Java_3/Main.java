38f1f649290ef3ccee697db11f22a4539b60d530
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-07 08:53:30 +0900

    [update] simplify lambda expression

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -2,10 +2,10 @@ import java.util.function.*;
 
 public class Main {
   public static void main(String[] args) {
-    Function<String, Integer> func = (String s) -> {
-      return s.length();
-    };
-    // int n = func.apply("Java");
+    Function<String, Integer> func = s -> s.length();
+    // Function<String, Integer> func = (String s) -> {
+    // return s.length();
+    // };
     System.out.println("文字列「Java」は" + func.apply("Java") + "文字です");
   }
 }
