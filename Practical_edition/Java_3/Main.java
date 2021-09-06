20281578eb8d2f69f4f9f66363c4d923a0114de2
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-07 08:45:38 +0900

    [add] Main.java with lambda expression

---
+++ Practical_edition/Java_3/Main.java
@@ -0,0 +1,11 @@
+import java.util.function.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Function<String, Integer> func = (String s) -> {
+      return s.length();
+    };
+    // int n = func.apply("Java");
+    System.out.println("文字列「Java」は" + func.apply("Java") + "文字です");
+  }
+}
