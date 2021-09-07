b192b22f4505a191228dd0f5477a3891beed02d5
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-07 15:01:24 +0900

    [add] exercise 3-4

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.function.*;
 
 public class Main {
@@ -28,11 +29,18 @@ public class Main {
     // System.out.println(f2.call(66, "Smith"));
 
     // 練習問題3-3
-    IntPredicate f1 = x -> x % 2 == 1;
-    Func2 f2 = (point, name) -> {
-      return name + "さんは" + (point > 65 ? "合格" : "不合格");
-    };
-    System.out.println(f1.test(15));
-    System.out.println(f2.call(66, "Smith"));
+    // IntPredicate f1 = x -> x % 2 == 1;
+    // Func2 f2 = (point, name) -> {
+    //   return name + "さんは" + (point > 65 ? "合格" : "不合格");
+    // };
+    // System.out.println(f1.test(15));
+    // System.out.println(f2.call(66, "Smith"));
+
+    // 練習問題3-4
+    List<String> names = List.of("湊雄輔", "朝香あゆみ", "菅原拓真", "大江岳人");
+    names.stream()
+      .filter(n -> n.length() <= 4)
+      .map(n -> n + "さん")
+      .forEach(System.out::println);
   }
 }
