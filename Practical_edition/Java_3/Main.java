e316675cde279d04eb7eb77b185fa600bbcba4ce
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-07 13:38:14 +0900

    [add] exercise 3-1

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -2,10 +2,14 @@ import java.util.function.*;
 
 public class Main {
   public static void main(String[] args) {
-    Function<String, Integer> func = s -> s.length();
-    // Function<String, Integer> func = (String s) -> {
-    // return s.length();
-    // };
-    System.out.println("文字列「Java」は" + func.apply("Java") + "文字です");
+    // Function<String, Integer> func = s -> s.length();
+    // System.out.println("文字列「Java」は" + func.apply("Java") + "文字です");
+
+    // 練習問題3-1
+    FuncList funclist = new FuncList();
+    Func1 f1 = FuncList::isOdd;
+    Func2 f2 = funclist::passCheck;
+    System.out.println(f1.call(15));
+    System.out.println(f2.call(66, "Smith"));
   }
 }
