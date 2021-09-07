388d3b88cef4388cb80d288afce692e443f37a11
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-07 14:32:12 +0900

    [add] exercise 3-2

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -7,16 +7,24 @@ public class Main {
 
     // 練習問題3-1
     // --独自インタフェースver-------------
-    Func1 f1 = FuncList::isOdd;
-    FuncList funclist = new FuncList(); // Func2のpassCheckはstaticではないため、まずインスタンス化
-    Func2 f2 = funclist::passCheck;
-    System.out.println(f1.call(15));
-    System.out.println(f2.call(66, "Smith"));
+    // Func1 f1 = FuncList::isOdd;
+    // FuncList funclist = new FuncList(); // Func2のpassCheckはstaticではないため、まずインスタンス化
+    // Func2 f2 = funclist::passCheck;
+    // System.out.println(f1.call(15));
+    // System.out.println(f2.call(66, "Smith"));
     // --標準関数インタフェースver---------
     // Predicate<Integer> f1 = FuncList::isOdd;
     // FuncList funclist = new FuncList();
     // BiFunction<Integer, String, String> f2 = funclist::passCheck;
     // System.out.println(f1.test(15));
     // System.out.println(f2.apply(66, "Smith"));
+
+    // 練習問題3-2
+    Func1 f1 = x -> x % 2 == 1;
+    Func2 f2 = (point, name) -> {
+      return name + "さんは" + (point > 65 ? "合格" : "不合格");
+    };
+    System.out.println(f1.call(15));
+    System.out.println(f2.call(66, "Smith"));
   }
 }
