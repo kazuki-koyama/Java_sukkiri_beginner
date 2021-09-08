269629bdd54baa76dffd76eeb2c542967a8b806d
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-09 07:29:30 +0900

    [add] read file with FileReader

--- Practical_edition/Java_6/Main.java
+++ Practical_edition/Java_6/Main.java
@@ -6,5 +6,16 @@ public class Main {
     fw.write('A');
     fw.flush();
     fw.close();
+
+    FileReader fw2 = new FileReader("rpgsave.dat");
+    System.out.println("すべてのデータを読んで表示します");
+    int i = fw2.read();
+    while (i != -1) {
+      char c = (char) i;
+      System.out.print(c);
+      i = fw2.read();
+    }
+    System.out.println("ファイルの末尾に到達しました");
+    fw2.close();
   }
 }
