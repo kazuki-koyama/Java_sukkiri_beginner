9bc166fb6c1fb3e7ee9aaf8b4fcf3eb008727535
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-13 00:00:00 +0900

    [add] get URL program with Socket class

--- Practical_edition/Java_8/Main.java
+++ Practical_edition/Java_8/Main.java
@@ -3,14 +3,30 @@ import java.net.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    URL url = new URL("https://dokojava.jp");
-    InputStream is = url.openStream();
+    // ▼Webページ読み込み(URLクラス)
+    // URL url = new URL("https://dokojava.jp");
+    // InputStream is = url.openStream();
+    // InputStreamReader isr = new InputStreamReader(is);
+    // int i = isr.read();
+    // while (i != -1) {
+    // System.out.print((char) i);
+    // i = isr.read();
+    // }
+    // isr.close();
+
+    // ▼Webページ読み込み(Socketクラス)
+    Socket sock = new Socket("dokojava.jp", 80);
+    InputStream is = sock.getInputStream();
+    OutputStream os = sock.getOutputStream();
+    os.write("GET /index.html HTTP/1.0\r\n".getBytes());
+    os.write("\r\n".getBytes());
+    os.flush();
     InputStreamReader isr = new InputStreamReader(is);
     int i = isr.read();
     while (i != -1) {
       System.out.print((char) i);
       i = isr.read();
     }
-    isr.close();
+    sock.close();
   }
 }
