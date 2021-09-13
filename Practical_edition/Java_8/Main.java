c03e2d297bbb4940debe1eedc29dc954b5d1dbf7
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-14 00:00:00 +0900

    [add] server program

--- Practical_edition/Java_8/Main.java
+++ Practical_edition/Java_8/Main.java
@@ -15,18 +15,27 @@ public class Main {
     // isr.close();
 
     // ▼Webページ読み込み(Socketクラス)
-    Socket sock = new Socket("dokojava.jp", 80);
-    InputStream is = sock.getInputStream();
-    OutputStream os = sock.getOutputStream();
-    os.write("GET /index.html HTTP/1.0\r\n".getBytes());
-    os.write("\r\n".getBytes());
-    os.flush();
-    InputStreamReader isr = new InputStreamReader(is);
-    int i = isr.read();
-    while (i != -1) {
-      System.out.print((char) i);
-      i = isr.read();
-    }
+    // Socket sock = new Socket("dokojava.jp", 80);
+    // InputStream is = sock.getInputStream();
+    // OutputStream os = sock.getOutputStream();
+    // os.write("GET /index.html HTTP/1.0\r\n".getBytes());
+    // os.write("\r\n".getBytes());
+    // os.flush();
+    // InputStreamReader isr = new InputStreamReader(is);
+    // int i = isr.read();
+    // while (i != -1) {
+    // System.out.print((char) i);
+    // i = isr.read();
+    // }
+    // sock.close();
+
+    // ▼サーバプログラム
+    System.out.println("起動完了");
+    ServerSocket svSock = new ServerSocket(39648);
+    Socket sock = svSock.accept();
+    System.out.println(sock.getInetAddress() + "から接続");
+    sock.getOutputStream().write("WELCOME".getBytes());
+    sock.getOutputStream().flush();
     sock.close();
   }
 }
