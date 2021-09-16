9632ead2a5e774e6f017547e713dc8f98fc5cf57
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-17 00:00:00 +0900

    [add] exercise 8-3

--- Practical_edition/Java_8/Main.java
+++ Practical_edition/Java_8/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import com.fasterxml.jackson.databind.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -52,10 +53,33 @@ public class Main {
     // os.close();
 
     // 練習問題8-2
-    Socket sock = new Socket("smtp.example.com", 60025);
-    OutputStream os = sock.getOutputStream();
-    os.write("HELO example.com\r\n".getBytes());
-    os.flush();
-    sock.close();
+    // Socket sock = new Socket("smtp.example.com", 60025);
+    // OutputStream os = sock.getOutputStream();
+    // os.write("HELO example.com\r\n".getBytes());
+    // os.flush();
+    // sock.close();
+
+    // 練習問題8-3
+    String url = "https://api.github.com/users/miyabilink";
+    HttpClient client = HttpClient.newBuilder()
+      .version(Version.HTTP_1_1)
+      .followRedirects(Redirect.NORMAL)
+      .build();
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .header("Accept", "application/vnd.github.v3+json")
+      .GET()
+      .build();
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    String body = res.body();
+    int status = res.statusCode();
+    if (status == 200) {
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode root = mapper.readTree(body);
+      String blog = root.get("blog").textValue();
+      System.out.println(blog);
+    } else {
+      System.out.println("ERROR: " + status);
+    }
   }
 }
