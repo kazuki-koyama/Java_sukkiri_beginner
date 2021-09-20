f8465f9d7dc0a74ee043f5f176a0099d6e846b96
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-21 07:15:32 +0900

    [add] sample 9-4

--- Practical_edition/Java_9/Main.java
+++ Practical_edition/Java_9/Main.java
@@ -15,21 +15,21 @@ public class Main {
       con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
 
       // STEP 2: 送信処理
-      // ********** コード9-3 ここから **********
       // STEP 2-(2)-1 送信すべきSQL文のひな形を準備
       PreparedStatement pstmt = con.prepareStatement
-          ("SELECT * FROM MONSTERS WHERE HP >= ?");
+          ("SELECT * FROM MONSTERS");
       // STEP 2-(2)-2 ひな形に値を流し込みSQL文を組み立てる
-      pstmt.setInt(1, 10);             // １番目の？に10を流し込む
+      // パラメータなし
       // STEP 2-(2)-3 組み立て終えたSQL文をDBMSに送信する
       ResultSet rs = pstmt.executeQuery();
       // STEP 2-(2)-4 結果表を処理する
-       /* *************************************
-          結果表の処理（記述する内容は、後述します）
-        ************************************* */
+      // ********** コード9-4 ここから **********
+      while (rs.next()) {
+        System.out.println(rs.getString("NAME"));
+      }
+      // ********** コード9-4 ここまで **********
       rs.close();
       pstmt.close();                  // 後片付け
-      // ********** コード9-3 ここまで **********
 
     } catch (SQLException e) {
       e.printStackTrace();
