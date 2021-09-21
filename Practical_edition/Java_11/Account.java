f9fe28f001d496c8835f966cd63ec5f55f7978de
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-21 15:23:51 +0900

    [add] Test Class for Account.java

---
+++ Practical_edition/Java_11/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+  String owner; // 口座名義人
+  int zandaka; // 口座残高
+
+  public Account(String owner, int zandaka) {
+    owner = owner;
+    zandaka = zandaka;
+  }
+
+  public void transfer(Account dest, int amount) {
+    dest.zandaka += amount;
+    zandaka -= amount;
+  }
+}
