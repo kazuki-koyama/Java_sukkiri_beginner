2d2cab4c81cacb6911b4c0e16412e0743c620718
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-21 15:25:23 +0900

    [fix] constructor of Account.java

--- Practical_edition/Java_11/Account.java
+++ Practical_edition/Java_11/Account.java
@@ -3,8 +3,8 @@ public class Account {
   int zandaka; // 口座残高
 
   public Account(String owner, int zandaka) {
-    owner = owner;
-    zandaka = zandaka;
+    this.owner = owner;
+    this.zandaka = zandaka;
   }
 
   public void transfer(Account dest, int amount) {
