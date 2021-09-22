3f07b6605f18d3ff0b3b0fc2fcef8d2ef10f6538
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-22 14:31:00 +0900

    [update] countdown program with parallel processing

--- Practical_edition/Java_16/Main.java
+++ Practical_edition/Java_16/Main.java
@@ -1,19 +1,13 @@
 import java.util.*;
-import java.util.concurrent.*;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("止めるには「STOP」を入力してください");
     System.out.println("カウントダウンを開始します");
-    for (int i = 9; i >= 0; i--) {
-      System.out.print(i + "..");
-      try {
-        TimeUnit.SECONDS.sleep(1);
-      } catch (InterruptedException e) {
-        ;
-      }
-    }
+    Thread t = new PrintingThread();
+    t.start();
     String input = new Scanner(System.in).nextLine();
     System.out.println("入力文字列：" + input);
+    System.out.println("停止処理は未作成です");
   }
 }
