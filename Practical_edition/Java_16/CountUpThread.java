8ab4ec461a226683c413e5bce15b56071f0295f5
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-22 17:30:08 +0900

    [add] exercise 16-1

---
+++ Practical_edition/Java_16/CountUpThread.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.*;
+
+public class CountUpThread extends Thread {
+  public void run() {
+    for (int i = 0; i <= 50; i++) {
+      System.out.println(i);
+    }
+  }
+}
--- Practical_edition/Java_16/Main.java
+++ Practical_edition/Java_16/Main.java
@@ -1,24 +1,29 @@
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("止めるには「STOP」を入力してください");
-    System.out.println("カウントダウンを開始します");
-    PrintingThread t = new PrintingThread();
-    t.start();
-    String input = new Scanner(System.in).nextLine();
+  public static void main(String[] args) throws Exception {
+    // System.out.println("止めるには「STOP」を入力してください");
+    // System.out.println("カウントダウンを開始します");
+    // PrintingThread t = new PrintingThread();
+    // t.start();
+    // String input = new Scanner(System.in).nextLine();
     // ▼カウントダウン中に、入力を受け付ける
     // System.out.println("入力文字列：" + input);
     // System.out.println("停止処理は未作成です");
 
     // ▼STOPが入力されたら、カウントダウンを中断
-    if (input.equals("STOP")) {
-      t.stopReq.set(true);
-    }
-    try {
-      t.join();
-    } catch (InterruptedException e) {
-      ;
-    }
+    // if (input.equals("STOP")) {
+    // t.stopReq.set(true);
+    // }
+    // try {
+    // t.join();
+    // } catch (InterruptedException e) {
+    // ;
+    // }
+
+    // ▼練習問題16-1
+    new CountUpThread().start();
+    new CountUpThread().start();
+    new CountUpThread().start();
   }
 }
