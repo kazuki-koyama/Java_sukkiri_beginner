4f59a29b4f180dfc3cb712e15f6685b40ab43166
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-09-23 08:45:45 +0900

    [add] exercise 17-1

--- Practical_edition/Java_17/Main.java
+++ Practical_edition/Java_17/Main.java
@@ -3,24 +3,37 @@ import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("はじめてのSwing");
+    // JFrame frame = new JFrame("はじめてのSwing");
+    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.setSize(400, 200);
+
+    // frame.setLayout(new BorderLayout());
+
+    // JLabel label = new JLabel("Hello World!!");
+    // frame.add(label, BorderLayout.CENTER);
+    // JButton buttonN = new JButton("上ボタン");
+    // frame.add(buttonN, BorderLayout.NORTH);
+    // JButton buttonS = new JButton("下ボタン");
+    // frame.add(buttonS, BorderLayout.SOUTH);
+    // JButton buttonW = new JButton("左ボタン");
+    // frame.add(buttonW, BorderLayout.WEST);
+    // JButton buttonE = new JButton("右ボタン");
+    // frame.add(buttonE, BorderLayout.EAST);
+
+    // frame.setVisible(true);
+    // System.out.println("フレームを表示");
+
+    // ▼練習問題17-1
+    JFrame frame = new JFrame("ログイン画面");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 200);
+    frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-    frame.setLayout(new BorderLayout());
-
-    JLabel label = new JLabel("Hello World!!");
-    frame.add(label, BorderLayout.CENTER);
-    JButton buttonN = new JButton("上ボタン");
-    frame.add(buttonN, BorderLayout.NORTH);
-    JButton buttonS = new JButton("下ボタン");
-    frame.add(buttonS, BorderLayout.SOUTH);
-    JButton buttonW = new JButton("左ボタン");
-    frame.add(buttonW, BorderLayout.WEST);
-    JButton buttonE = new JButton("右ボタン");
-    frame.add(buttonE, BorderLayout.EAST);
+    frame.add(new IDPanel());
+    frame.add(new PWPanel());
+    frame.add(new CheckPanel());
+    frame.add(new ButtonPanel());
 
     frame.setVisible(true);
-    System.out.println("フレームを表示");
   }
 }
